26ec1c49693817b07e8aeab4082de511030512df
Nikita <sorokovsky.ni@yandex.ru>
2022-12-14 13:10:32 +0300

    Done hometask41

---
+++ task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (Convert.ToInt32(Console.ReadLine()) > 0 )
+       count++;
+}
+Console.WriteLine(count);
